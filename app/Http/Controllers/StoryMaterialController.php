8e21a81c63aef8ece6135593b9f6109349224cd7
Тищенко Артем <Tischenko.Artem@kronas.com.ua>
2022-11-10 14:53:55 +0200

    fix story material

--- app/Http/Controllers/StoryMaterialController.php
+++ app/Http/Controllers/StoryMaterialController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cell;
+use App\Models\Decor;
 use App\Models\MaterialModel;
 use App\Models\StoryMaterialModel;
+use App\Models\TypeMaterialModel;
 use Illuminate\Http\Request;
 
 class StoryMaterialController extends Controller
@@ -12,18 +15,19 @@ class StoryMaterialController extends Controller
         $SM = new StoryMaterialModel();
         $material =  MaterialModel::find($materialId);
 
-        //$SM->id = 0;
+        //$SM->id = null;
         $SM->vendor_code = $material->vendor_code;
-        $SM->type_material_id = $material->type_material_id;
-        $SM->decor_id = $material->decor_id;
-        $SM->cell_id = $material->cell_id;
+        $SM->type_material = TypeMaterialModel::find($material->type_material_id)->name ;
+        $SM->decor = Decor::find($material->decor_id)->name;
+        $cell = Cell::find($material->cell_id);
+        $SM->cell = $cell->rack . '-'. $cell->storey . '-' . $cell->row;
         $SM->length = $material->length;
         $SM->width = $material->width;
         $SM->thickness = $material->thickness;
         $SM->created_at = $material->created_at;
         $SM->updated_at = $material->updated_at;
         $SM->accounting = $material->accounting;
-        $SM->code_user = $code_user;
+        $SM->kronas_user = $code_user;
         $SM->action_material_id = $action;
         dump($SM);
         $SM->save();
