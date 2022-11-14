4257dbf08559d07ccc8c3c71fd3a30ae84876426
Тищенко Артем <Tischenko.Artem@kronas.com.ua>
2022-11-14 15:43:21 +0200

    web decor

--- resources/views/decor.blade.php
+++ resources/views/decor.blade.php
@@ -11,24 +11,24 @@
                 <button class="w-100 btn btn-primary btn mt-3" type="submit">Search</button>
             </form>
 
-            <p class="mt-4">decor</p>
+            <p class="mt-4">Decor</p>
             <form class="needs-validation" id="setdecor"><!--action="api/v1/decor" method="post"-->
                 @csrf
                 <input placeholder="id" class="form-control" type="number" name="id">
-                <input placeholder="decor_name" class="form-control" type="text" name="decor_name">
+                <input placeholder="decor_name" class="form-control mt-2" type="text" name="decor_name">
                 <button class="w-100 btn btn-primary btn mt-3" type="submit">Add</button>
             </form>
         </div>
         <table class="table mt-4">
             <tr class="thead-dark">
-                <th scope="col">id</th>
-                <th scope="col">decor_name</th>
+                <th scope="col">Id</th>
+                <th scope="col">Decor name</th>
             </tr>
 
             @foreach($decor as $el)
                 <tr>
-                    <form method="de">
-                        <td name="id" >{{$el->id}}</td>
+                    <form action="" method="get">
+                        <td>{{$el->id}}</td>
                         <td>{{$el->decor_name}}</td>
                         <td style="max-width: 40px">
                             <button type="submit" class="w-100 btn btn-warning">edit</button>
@@ -62,12 +62,19 @@
 
                 success: function (response, u) {
                     console.log(response, u);
+                    //location.reload();
                     $.alert({
+                        theme: 'black',
                         type: 'green',
                         draggable: false,
                         title: response.status,
-                        content: response.message
+                        content: response.message,
+                        confirm: function () {
+                            location.reload();
+                        }
                     });
+                    // location.reload();
+
                 },
                 error: function (response, u, v) {
                     $.alert({
